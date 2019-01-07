2817c12a136386d1aaac0e3e4f041aa48b0bd28c
PATO <chiefpatnwafor@gmail.com>
2019-01-07 07:22:37 +0100

    pato

--- app/javascript/index.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("loanstic-firebase-adminsdk-38mis-c3a7df73d5.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://loanstic.firebaseio.com"
-});
