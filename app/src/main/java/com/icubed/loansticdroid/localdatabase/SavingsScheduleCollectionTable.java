03c2634ab988988dfeabe2078d6cac4ab0f1cfd6
PATO <chiefpatnwafor@gmail.com>
2019-03-25 05:24:16 +0100

    pato

---
+++ app/src/main/java/com/icubed/loansticdroid/localdatabase/SavingsScheduleCollectionTable.java
@@ -0,0 +1,144 @@
+package com.icubed.loansticdroid.localdatabase;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Unique;
+
+import java.util.Date;
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class SavingsScheduleCollectionTable {
+
+    @Unique
+    private String savingsScheduleId;
+
+    private String savingsId;
+
+    private int savingsCollectionNumber;
+
+    @Id(autoincrement = true)
+    private Long id;
+
+    private Double savingsCollectionAmount;
+
+    private Date savingsCollectionDueDate;
+    private Date lastUpdatedAt;
+    private Date timestamp;
+    private double amountPaid;
+    private String savingsCollectionState;
+
+    private Boolean isSavingsCollected;
+
+    @Generated(hash = 1127909122)
+    public SavingsScheduleCollectionTable(String savingsScheduleId, String savingsId,
+                                          int savingsCollectionNumber, Long id, Double savingsCollectionAmount,
+                                          Date savingsCollectionDueDate, Date lastUpdatedAt, Date timestamp,
+                                          double amountPaid, String savingsCollectionState,
+                                          Boolean isSavingsCollected) {
+        this.savingsScheduleId = savingsScheduleId;
+        this.savingsId = savingsId;
+        this.savingsCollectionNumber = savingsCollectionNumber;
+        this.id = id;
+        this.savingsCollectionAmount = savingsCollectionAmount;
+        this.savingsCollectionDueDate = savingsCollectionDueDate;
+        this.lastUpdatedAt = lastUpdatedAt;
+        this.timestamp = timestamp;
+        this.amountPaid = amountPaid;
+        this.savingsCollectionState = savingsCollectionState;
+        this.isSavingsCollected = isSavingsCollected;
+    }
+
+    @Generated(hash = 1506363868)
+    public SavingsScheduleCollectionTable() {
+    }
+
+    public String getSavingsScheduleId() {
+        return this.savingsScheduleId;
+    }
+
+    public void setSavingsScheduleId(String savingsScheduleId) {
+        this.savingsScheduleId = savingsScheduleId;
+    }
+
+    public String getSavingsId() {
+        return this.savingsId;
+    }
+
+    public void setSavingsId(String savingsId) {
+        this.savingsId = savingsId;
+    }
+
+    public int getSavingsCollectionNumber() {
+        return this.savingsCollectionNumber;
+    }
+
+    public void setSavingsCollectionNumber(int savingsCollectionNumber) {
+        this.savingsCollectionNumber = savingsCollectionNumber;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getSavingsCollectionAmount() {
+        return this.savingsCollectionAmount;
+    }
+
+    public void setSavingsCollectionAmount(Double savingsCollectionAmount) {
+        this.savingsCollectionAmount = savingsCollectionAmount;
+    }
+
+    public Date getSavingsCollectionDueDate() {
+        return this.savingsCollectionDueDate;
+    }
+
+    public void setSavingsCollectionDueDate(Date savingsCollectionDueDate) {
+        this.savingsCollectionDueDate = savingsCollectionDueDate;
+    }
+
+    public Date getLastUpdatedAt() {
+        return this.lastUpdatedAt;
+    }
+
+    public void setLastUpdatedAt(Date lastUpdatedAt) {
+        this.lastUpdatedAt = lastUpdatedAt;
+    }
+
+    public Date getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getAmountPaid() {
+        return this.amountPaid;
+    }
+
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public String getSavingsCollectionState() {
+        return this.savingsCollectionState;
+    }
+
+    public void setSavingsCollectionState(String savingsCollectionState) {
+        this.savingsCollectionState = savingsCollectionState;
+    }
+
+    public Boolean getIsSavingsCollected() {
+        return this.isSavingsCollected;
+    }
+
+    public void setIsSavingsCollected(Boolean isSavingsCollected) {
+        this.isSavingsCollected = isSavingsCollected;
+    }
+
+}
--- app/src/main/java/com/icubed/loansticdroid/localdatabase/SavingsScheduleTable.java
+++ app/src/main/java/com/icubed/loansticdroid/localdatabase/SavingsScheduleTable.java
@@ -11,134 +11,141 @@ import org.greenrobot.greendao.annotation.Generated;
 public class SavingsScheduleTable {
 
     @Unique
-    private String savingsScheduleId;
-
     private String savingsId;
 
-    private int savingsCollectionNumber;
-
     @Id(autoincrement = true)
-    private Long id;
+    private Long Id;
 
-    private Double savingsCollectionAmount;
+    private String borrowerId, groupId;
 
-    private Date savingsCollectionDueDate;
-    private Date lastUpdatedAt;
-    private Date timestamp;
-    private double amountPaid;
-    private String savingsCollectionState;
-
-    private Boolean isSavingsCollected;
-
-    @Generated(hash = 1127909122)
-    public SavingsScheduleTable(String savingsScheduleId, String savingsId,
-            int savingsCollectionNumber, Long id, Double savingsCollectionAmount,
-            Date savingsCollectionDueDate, Date lastUpdatedAt, Date timestamp,
-            double amountPaid, String savingsCollectionState,
-            Boolean isSavingsCollected) {
-        this.savingsScheduleId = savingsScheduleId;
+    private double savingsAmount;
+    private double savingsInterestRate, amountSaved;
+    private Date savingsCreationDate;
+    private Date savingsApprovedDate, lastUpdatedAt;
+    private int savingsDuration;
+    private String loanOfficerId, savingsInterestRateUnit
+            , savingsDurationUnit, savingsAmountUnit, savingsNumber;
+    @Generated(hash = 1525900887)
+    public SavingsScheduleTable(String savingsId, Long Id, String borrowerId,
+                                String groupId, double savingsAmount, double savingsInterestRate,
+                                double amountSaved, Date savingsCreationDate, Date savingsApprovedDate,
+                                Date lastUpdatedAt, int savingsDuration, String loanOfficerId,
+                                String savingsInterestRateUnit, String savingsDurationUnit,
+                                String savingsAmountUnit, String savingsNumber) {
         this.savingsId = savingsId;
-        this.savingsCollectionNumber = savingsCollectionNumber;
-        this.id = id;
-        this.savingsCollectionAmount = savingsCollectionAmount;
-        this.savingsCollectionDueDate = savingsCollectionDueDate;
+        this.Id = Id;
+        this.borrowerId = borrowerId;
+        this.groupId = groupId;
+        this.savingsAmount = savingsAmount;
+        this.savingsInterestRate = savingsInterestRate;
+        this.amountSaved = amountSaved;
+        this.savingsCreationDate = savingsCreationDate;
+        this.savingsApprovedDate = savingsApprovedDate;
         this.lastUpdatedAt = lastUpdatedAt;
-        this.timestamp = timestamp;
-        this.amountPaid = amountPaid;
-        this.savingsCollectionState = savingsCollectionState;
-        this.isSavingsCollected = isSavingsCollected;
+        this.savingsDuration = savingsDuration;
+        this.loanOfficerId = loanOfficerId;
+        this.savingsInterestRateUnit = savingsInterestRateUnit;
+        this.savingsDurationUnit = savingsDurationUnit;
+        this.savingsAmountUnit = savingsAmountUnit;
+        this.savingsNumber = savingsNumber;
     }
-
-    @Generated(hash = 1506363868)
+    @Generated(hash = 1055947197)
     public SavingsScheduleTable() {
     }
-
-    public String getSavingsScheduleId() {
-        return this.savingsScheduleId;
-    }
-
-    public void setSavingsScheduleId(String savingsScheduleId) {
-        this.savingsScheduleId = savingsScheduleId;
-    }
-
     public String getSavingsId() {
         return this.savingsId;
     }
-
     public void setSavingsId(String savingsId) {
         this.savingsId = savingsId;
     }
-
-    public int getSavingsCollectionNumber() {
-        return this.savingsCollectionNumber;
-    }
-
-    public void setSavingsCollectionNumber(int savingsCollectionNumber) {
-        this.savingsCollectionNumber = savingsCollectionNumber;
-    }
-
     public Long getId() {
-        return this.id;
+        return this.Id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long Id) {
+        this.Id = Id;
     }
-
-    public Double getSavingsCollectionAmount() {
-        return this.savingsCollectionAmount;
+    public String getBorrowerId() {
+        return this.borrowerId;
     }
-
-    public void setSavingsCollectionAmount(Double savingsCollectionAmount) {
-        this.savingsCollectionAmount = savingsCollectionAmount;
+    public void setBorrowerId(String borrowerId) {
+        this.borrowerId = borrowerId;
     }
-
-    public Date getSavingsCollectionDueDate() {
-        return this.savingsCollectionDueDate;
+    public String getGroupId() {
+        return this.groupId;
     }
-
-    public void setSavingsCollectionDueDate(Date savingsCollectionDueDate) {
-        this.savingsCollectionDueDate = savingsCollectionDueDate;
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
     }
-
+    public double getSavingsAmount() {
+        return this.savingsAmount;
+    }
+    public void setSavingsAmount(double savingsAmount) {
+        this.savingsAmount = savingsAmount;
+    }
+    public double getSavingsInterestRate() {
+        return this.savingsInterestRate;
+    }
+    public void setSavingsInterestRate(double savingsInterestRate) {
+        this.savingsInterestRate = savingsInterestRate;
+    }
+    public double getAmountSaved() {
+        return this.amountSaved;
+    }
+    public void setAmountSaved(double amountSaved) {
+        this.amountSaved = amountSaved;
+    }
+    public Date getSavingsCreationDate() {
+        return this.savingsCreationDate;
+    }
+    public void setSavingsCreationDate(Date savingsCreationDate) {
+        this.savingsCreationDate = savingsCreationDate;
+    }
+    public Date getSavingsApprovedDate() {
+        return this.savingsApprovedDate;
+    }
+    public void setSavingsApprovedDate(Date savingsApprovedDate) {
+        this.savingsApprovedDate = savingsApprovedDate;
+    }
     public Date getLastUpdatedAt() {
         return this.lastUpdatedAt;
     }
-
     public void setLastUpdatedAt(Date lastUpdatedAt) {
         this.lastUpdatedAt = lastUpdatedAt;
     }
-
-    public Date getTimestamp() {
-        return this.timestamp;
+    public int getSavingsDuration() {
+        return this.savingsDuration;
     }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
+    public void setSavingsDuration(int savingsDuration) {
+        this.savingsDuration = savingsDuration;
     }
-
-    public double getAmountPaid() {
-        return this.amountPaid;
+    public String getLoanOfficerId() {
+        return this.loanOfficerId;
     }
-
-    public void setAmountPaid(double amountPaid) {
-        this.amountPaid = amountPaid;
+    public void setLoanOfficerId(String loanOfficerId) {
+        this.loanOfficerId = loanOfficerId;
     }
-
-    public String getSavingsCollectionState() {
-        return this.savingsCollectionState;
+    public String getSavingsInterestRateUnit() {
+        return this.savingsInterestRateUnit;
     }
-
-    public void setSavingsCollectionState(String savingsCollectionState) {
-        this.savingsCollectionState = savingsCollectionState;
+    public void setSavingsInterestRateUnit(String savingsInterestRateUnit) {
+        this.savingsInterestRateUnit = savingsInterestRateUnit;
     }
-
-    public Boolean getIsSavingsCollected() {
-        return this.isSavingsCollected;
+    public String getSavingsDurationUnit() {
+        return this.savingsDurationUnit;
     }
-
-    public void setIsSavingsCollected(Boolean isSavingsCollected) {
-        this.isSavingsCollected = isSavingsCollected;
+    public void setSavingsDurationUnit(String savingsDurationUnit) {
+        this.savingsDurationUnit = savingsDurationUnit;
     }
-
+    public String getSavingsAmountUnit() {
+        return this.savingsAmountUnit;
+    }
+    public void setSavingsAmountUnit(String savingsAmountUnit) {
+        this.savingsAmountUnit = savingsAmountUnit;
+    }
+    public String getSavingsNumber() {
+        return this.savingsNumber;
+    }
+    public void setSavingsNumber(String savingsNumber) {
+        this.savingsNumber = savingsNumber;
+    }
 }
