a7ac0d0370f5ab28b8d7c6c3560afae3cca1fe9a
EMASCO <emascochukwujindu@gmail.com>
2019-03-25 10:41:51 +0100

    emasco

--- app/src/main/java/com/icubed/loansticdroid/activities/CollectionActivity.java
+++ app/src/main/java/com/icubed/loansticdroid/activities/CollectionActivity.java
@@ -2,14 +2,26 @@ package com.icubed.loansticdroid.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.icubed.loansticdroid.R;
+import com.ramotion.foldingcell.FoldingCell;
 
 public class CollectionActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
+
+       toolbar = findViewById(R.id.collection_activity_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Collection");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
     }
 }
