dd4bd77b0a89ce37720508037e91ea3da45ad27d
PATO <chiefpatnwafor@gmail.com>
2019-03-26 20:30:02 +0100

    pato

--- app/src/main/java/com/icubed/loansticdroid/fragments/RepaymentFragment/SavingsPaymentFragment.java
+++ app/src/main/java/com/icubed/loansticdroid/fragments/RepaymentFragment/SavingsPaymentFragment.java
@@ -2,14 +2,18 @@ package com.icubed.loansticdroid.fragments.RepaymentFragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.icubed.loansticdroid.R;
 import com.icubed.loansticdroid.adapters.LoanRepaymentRecyclerAdapter;
@@ -18,7 +22,9 @@ import com.icubed.loansticdroid.models.LoanDetails;
 import com.icubed.loansticdroid.models.LoanRepayment;
 import com.icubed.loansticdroid.models.Savings;
 import com.icubed.loansticdroid.models.SavingsDetails;
+import com.icubed.loansticdroid.util.AndroidUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,4 +52,53 @@ public class SavingsPaymentFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_savings_payment, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.savings_list);
+        emptyCollection = view.findViewById(R.id.search_empty_layout);
+        progressBar = view.findViewById(R.id.progressBar);
+
+        savings = new Savings(getActivity());
+
+        //Swipe down refresher initialization
+        swipeRefreshLayout = view.findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light,
+                android.R.color.holo_green_dark,
+                R.color.colorAccent);
+        swipeRefreshListener();
+
+        savingsDetailsList = new ArrayList<>();
+        savingsRecyclerAdapter = new SavingsRecyclerAdapter(savingsDetailsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter((savingsRecyclerAdapter));
+
+        getAllSavings();
+    }
+
+    //Swipe down refresh lstener
+    private void swipeRefreshListener() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if(AndroidUtils.isMobileDataEnabled(getContext())) {
+                    swipeRefreshLayout.setRefreshing(true);
+                    savings.loadAllSavingssAndCompareToLocal();
+                }else {
+                    Toast.makeText(getContext(), "Request failed, please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void getAllSavings() {
+        if(!savings.doesSavingsTableExistInLocalStorage()){
+            savings.loadAllSavings();
+        }else{
+            swipeRefreshLayout.setRefreshing(true);
+            savings.loadSavingsToUI();
+            savings.loadAllSavingssAndCompareToLocal();
+        }
+    }
 }
