d404726eed49990a277b84c1237468b0cbb8adf9
EMASCO <emascochukwujindu@gmail.com>
2019-03-27 11:40:51 +0100

    emasco

---
+++ app/src/main/java/com/icubed/loansticdroid/activities/LifeGoalsSetup6GoalSummary.java
@@ -0,0 +1,15 @@
+package com.icubed.loansticdroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.icubed.loansticdroid.R;
+
+public class LifeGoalsSetup6GoalSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_goals_setup6_goal_summary);
+    }
+}
